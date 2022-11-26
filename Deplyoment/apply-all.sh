9bddb95fa307326be7ef22428268f1ade06e0146
Mahmoud Ashraf <mahmoud.ashraf.cis@gmail.com>
2022-11-26 08:35:44 +0200

    sh edit

--- Deplyoment/apply-all.sh
+++ Deplyoment/apply-all.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-kubectl create namespace devops-tools &&
-kubectl apply -f serviceAccount.yaml &&
-kubectl create -f volume.yaml &&
-kubectl apply -f deployment.yaml &&
-kubectl apply -f service.yaml &&
+kubectl create namespace devops-tools 
+kubectl apply -f serviceAccount.yaml 
+kubectl create -f volume.yaml 
+kubectl apply -f deployment.yaml 
+kubectl apply -f service.yaml 
 
-
-kubectl get deployments -n devops-tools &&
+kubectl get deployments -n devops-tools 
 kubectl  describe deployments --namespace=devops-tools

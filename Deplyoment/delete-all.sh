47837c3b4d70bfe8a9c4cdd0991447f71f3aa9cb
Mahmoud Ashraf <mahmoud.ashraf.cis@gmail.com>
2022-11-26 08:21:57 +0200

    Update delete-all.sh

--- Deplyoment/delete-all.sh
+++ Deplyoment/delete-all.sh
@@ -3,4 +3,4 @@ kubectl delete namespace devops-tools &&
 kubectl delete -f serviceAccount.yaml &&
 kubectl delete -f volume.yaml &&
 kubectl delete -f deployment.yaml &&
-kubectl delete -f service.yaml &&
+kubectl delete -f service.yaml 

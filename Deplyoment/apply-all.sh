72e42cb8a4ede83d064e018b9a741759a23ac5c9
Mahmoud Ashraf <mahmoud.ashraf.cis@gmail.com>
2022-11-26 08:19:17 +0200

    adding sh scripts

---
+++ Deplyoment/apply-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+kubectl create namespace devops-tools &&
+kubectl apply -f serviceAccount.yaml &&
+kubectl create -f volume.yaml &&
+kubectl apply -f deployment.yaml &&
+kubectl apply -f service.yaml &&
+
+
+kubectl get deployments -n devops-tools &&
+kubectl  describe deployments --namespace=devops-tools
